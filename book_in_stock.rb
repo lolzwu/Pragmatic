c171bc4a447f16b685f1c7c1c170d19a5259d287
lolzwu <wtf@gmail.com>
2012-10-21 23:17:48 +0800

    ruby attributes sample

--- book_in_stock.rb
+++ book_in_stock.rb
@@ -8,49 +8,20 @@ class BookInStock
     @price = Float(price)
   end
 
-  def to_s
-    "ISBN: #{@isbn}, price: #{@price}"
+  def price_in_cents
+    Integer(price*100 + 0.5)
   end
 
-=begin
-  def price=(new_price)
-    @price = new_price
+  def price_in_cents=(cents)
+    @price = cents / 100.0
   end
-=end
-=begin 改用attr_* 取代
-  def isbn
-    @isbn
-  end
-
-  def price
-    @price
-  end
-=end
-
 
 end
 
 
 book = BookInStock.new("isbn1", 33.80)
-puts "ISBN = #{book.isbn}"
 puts "Price = #{book.price}"
-book.price = book.price * 0.75
-puts "New price = #{book.price}"
-
-=begin
-book = BookInStock.new("isbn1", 12.34)
-puts "ISBN = #{book.isbn}"
+puts "Price in cents = #{book.price_in_cents}"
+book.price_in_cents = 1234
 puts "Price = #{book.price}"
-
-
-b1 = BookInStock.new("temp1",3)
-b2 = BookInStock.new("temp2",3.14)
-b3 = BookInStock.new("temp4",3.15)
-
-p b1
-puts b1
-p b2
-puts b2
-p b3
-puts b3
-=end
+puts "Price in cents = #{book.price_in_cents}"
